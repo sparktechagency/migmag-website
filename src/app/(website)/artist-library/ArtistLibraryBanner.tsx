b4e98312126ba8fc425aa88cba287a653b2aec15
ishanrana363 <ishanrana363@gmail.com>
2025-06-13 12:11:25 -0800

    artist page design modify

--- src/app/(website)/artist-library/ArtistLibraryBanner.tsx
+++ src/app/(website)/artist-library/ArtistLibraryBanner.tsx
@@ -1,45 +1,58 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import MaxWidth from "@/components/max-width/MaxWidth";
 
 const ArtistLibraryBanner = () => {
     return (
-        <div className=' max-w-[1539px] mx-auto px-4 mt-8   ' >
-            <div className=' ' >
-                <div className='  flex flex-col lg:flex-row justify-between ' >
-                    {/* left side  */}
-                    <div className=' max-w-2xl text-white ' >
-                        <div style={{ fontFamily: 'Bayon' }} className=' max-w-[633px] lg:text-[60px] text-4xl lg:leading-24 ' >
-                            <h1>TALENTED ARTISTS AVAILABLE FOR YOUR TRACKS <span className={`text-[#E7F056]`} > - TuneM Artists </span> </h1>
+        <div className=' mx-auto mt-8   '>
+            <MaxWidth>
+                <div className=' '>
+                    <div className='  flex flex-col lg:flex-row justify-between '>
+                        {/* left side  */}
+                        <div className=' max-w-2xl text-white '>
+                            <div style={{fontFamily: 'Bayon'}}
+                                 className=' max-w-[633px] lg:text-[60px] text-4xl lg:leading-24 '>
+                                <h1>TALENTED ARTISTS AVAILABLE FOR YOUR TRACKS <span className={`text-[#E7F056]`}> - TuneM Artists </span>
+                                </h1>
+                            </div>
+                            <div style={{fontFamily: 'Favorit'}}
+                                 className=' max-w-[436px] mt-3 lg:mt-10 text-lg leading-6 font-bold '>
+                                <p>
+                                    Looking for someone with that something special? Contact us and let us know what
+                                    you’re after, and we’ll search the world for you! -
+                                </p>
+                                <p>
+                                </p>
+                            </div>
+
+                            <div style={{fontFamily: 'Favorit'}}
+                                 className=' flex gap-3.5 lg:gap-12 items-center mb-6 lg:mt-[68px] mt-8 '>
+                                <Link className=' cursor-pointer ' href={"/contact"}>
+                                    <button
+                                        className=' cursor-pointer text-[#E7F056] text-[15px] border border-[#E7F056] rounded-2xl px-4 py-2  '>CONTACT
+                                        US
+                                    </button>
+                                </Link>
+                            </div>
                         </div>
-                        <div style={{ fontFamily: 'Favorit' }} className=' max-w-[436px] mt-3 lg:mt-10 text-lg leading-6 font-bold ' >
-                            <p>
-                                Looking for someone with that something special? Contact us and let us know what you’re after, and we’ll search the world for you! -
-                            </p>
-                            <p>
-                            </p>
-                        </div>
-                        
-                        <div style={{ fontFamily: 'Favorit' }}  className=' flex gap-3.5 lg:gap-12 items-center mb-6 lg:mt-[68px] mt-8 ' >
-                            <Link className=' cursor-pointer ' href={"/contact"}><button className=' cursor-pointer text-[#E7F056] text-lg border border-[#E7F056] rounded-2xl px-7 py-2  ' >CONTACT US</button></Link>
+                        {/* right side  */}
+                        <div>
+                            <Image
+                                src="/images/browse-vocal/banner/browse-vocal-banner.jpg"
+                                width={772}
+                                height={478}
+                                alt="Browse Vocal Banner"
+                                className=' object-cover rounded  '
+                            />
+
                         </div>
                     </div>
-                    {/* right side  */}
                     <div>
-                        <Image
-                            src="/images/browse-vocal/banner/browse-vocal-banner.jpg"
-                            width={772}
-                            height={478}
-                            alt="Browse Vocal Banner"
-                            className=' object-cover rounded  '
-                        />
 
                     </div>
                 </div>
-                <div>
-
-                </div>
-            </div>
+            </MaxWidth>
         </div>
     )
 }
